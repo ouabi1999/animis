f9f6823ea22226d98ce950df45a9fb18a606defb
ouabi1999 <ouabiabdessamad@gmail.com>
2022-07-12 17:05:56 +0100

    change sqlite to postgressql

--- back-end/api/__init__.py
+++ back-end/api/__init__.py
@@ -19,20 +19,19 @@ server_session = Session()
 def create_app():
     app = Flask(__name__)
     bcrypt.init_app(app)
-    db.init_app(app)
     cors.init_app(app, supports_credentials=True)
-    db.init_app(app)
-
     migrate.init_app(app, db)
     app.secret_key = "mysecretket"
     app.config['SESSION_PERMANENT'] = True
     app.config['SESSION_TYPE'] = 'sqlalchemy'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:mina9991rwina@localhost/corazondata"
     app.config['SESSION_USE_SIGNER'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['SESSION_SQLALCHEMY'] = db
     app.config['SQLALCHEMY_ECHO'] = True  
     app.config['PRODUCTS_IMAGES'] = PRODUCTS_IMAGES   
     server_session.init_app(app)
+    db.init_app(app)
     ################################################
     from  .views import views
     from  .auth import auth
@@ -42,12 +41,13 @@ def create_app():
     app.register_blueprint(payment)
    
 
-    #create_database(app)
+    db.create_all(app = app)
     #db.create_all(app = app)
     return app
 
 
-def create_database(app):
-    
-    db.create_all(app=app)
-    print('Created Database!')
+
+
+
+
+
