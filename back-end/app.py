ea84de1bc5184d7c272348b6bf6f9e8375face54
ouabi1999 <ouabiabdessamad@gmail.com>
2023-03-08 14:57:41 +0100

    test some changes

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,13 @@
 from .api import create_app, socketio
 from flask import send_from_directory
 app = create_app()
+
 @app.route("/")
+
 def serve():
-  
-    return send_from_directory(app.static_folder, "index.html")
+    #return send_from_directory(app.static_folder, "index.html")
+    return {"error": "yes of course"}
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
 
