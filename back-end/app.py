f443d07b060cc16c1667b7abd7fcbd5bddc95cdb
ouabi1999 <ouabiabdessamad@gmail.com>
2023-03-09 03:01:41 +0100

    fix another one

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,6 @@
 from api import  create_app, socketio
 from flask import send_from_directory
+import os
 app = create_app()
 
 @app.route("/")
@@ -7,8 +8,11 @@ app = create_app()
 def serve():
     #return send_from_directory(app.static_folder, "index.html")
     return {"error": "yes of course"}
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
