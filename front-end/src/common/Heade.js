cf9998d6b5712f286ed2763623316b446490f25a
ouabi1999 <ouabiabdessamad@gmail.com>
2023-03-17 01:11:03 +0100

    add helmet async

--- front-end/src/common/Heade.js
+++ front-end/src/common/Heade.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Helmet } from 'react-helmet';
+import { Helmet} from 'react-helmet-async';
 
 function HeadeSeo({title}) {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta name="description" content="Animis Shop is your one-stop-shop for all things anime. We offer a wide selection of anime merchandise, including figurines, clothing, accessories, and more." />
-      <meta name="keywords" content="animis shop, animis,  anime toys, anime stickers, anime merchandise, anime figurines, anime clothing, anime accessories" />
     </Helmet>
   )
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,14 +4,16 @@ import { Provider } from 'react-redux';
 import {store} from "./app/store"
 import App from './App';
 import "./App.css"
+import {HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
-  
+  <HelmetProvider>
     <Provider store={store}>
       <Fragment>
         <App />
       </Fragment>
       </Provider>
+    </HelmetProvider>
  ,document.getElementById('root')
 );
 
